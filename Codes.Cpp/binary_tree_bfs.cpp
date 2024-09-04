932eefdb99cde6d3df4ebfbe1a86408afcb32c8d
anka <1350989414@qq.com>
2024-09-04 12:26:12 +0800

    初始化

---
+++ Codes.Cpp/binary_tree_bfs.cpp
@@ -0,0 +1,18 @@
+/* 层序遍历 */
+vector<int> levelOrder(TreeNode *root) {
+    // 初始化队列，加入根节点
+    queue<TreeNode *> queue;
+    queue.push(root);
+    // 初始化一个列表，用于保存遍历序列
+    vector<int> vec;
+    while (!queue.empty()) {
+        TreeNode *node = queue.front();
+        queue.pop();              // 队列出队
+        vec.push_back(node->val); // 保存节点值
+        if (node->left != nullptr)
+            queue.push(node->left); // 左子节点入队
+        if (node->right != nullptr)
+            queue.push(node->right); // 右子节点入队
+    }
+    return vec;
+}
